3a5b0b147fb304237cffd1338e78525b64ba01f5
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-07-04 08:16:18 +0200

    settings django.contrib.auth is on

--- project_core/settings.py
+++ project_core/settings.py
@@ -42,7 +42,7 @@ LOGIN_REDIRECT_URL = '/docs/'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    #'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
